a971198e2bfcadfd2181fc63b7c7d23a2f1b843c
tonishii <anthonylibut@gmail.com>
2025-10-11 11:38:58 +0800

    Added import file and detailed setup in README.

---
+++ scripts/import_imdb.sh
@@ -0,0 +1,25 @@
+# Import IMDb TSV files into PostgreSQL
+DB_HOST="stadvdb-source-database.cvwaeiiws18z.ap-southeast-1.rds.amazonaws.com"
+DB_NAME="stadvdb_source_db"
+DB_USER="postgres"
+DB_SCHEMA="stadvdb"
+IMPORT_DIR="/home/ec2-user/imports"
+
+echo "Starting data import..."
+
+import_tsv() {
+  local table=$1
+  local file=$2
+  echo "Importing $file into $table..."
+  psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c "\copy $DB_SCHEMA.$table FROM '$IMPORT_DIR/$file' WITH (FORMAT text, DELIMITER E'\t', NULL '\N', HEADER)"
+}
+
+import_tsv "akas_import" "title.akas.tsv"
+import_tsv "title_basics" "title.basics.tsv"
+import_tsv "crew_import" "title.crew.tsv"
+import_tsv "episode" "title.episode.tsv"
+import_tsv "ratings" "title.ratings.tsv"
+import_tsv "name_basics_import" "name.basics.tsv"
+import_tsv "principals" "title.principals.tsv"
+
+echo "Imports completed successfully ✓"
